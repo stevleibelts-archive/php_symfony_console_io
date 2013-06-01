4f1b6dd66287b98c3001619156246c93a52ddd7b
stevleibelt <stev@leibelt.de>
2013-06-01 19:17:41 +0200

    Added InputException

---
+++ source/Net/Bazzline/Symfony/Console/IO/InputException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-01 
+ */
+
+namespace Net\Bazzline\Symfony\Console\IO;
+
+use InvalidArgumentException;
+
+/**
+ * Class InputException
+ *
+ * @package Net\Bazzline\Symfony\Console\IO
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-01
+ */
+class InputException extends InvalidArgumentException {}
